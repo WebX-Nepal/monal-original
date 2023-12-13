25448e07db6bf6623aa41b5780e641bdb589911e
AshishLawaju <ashish.lawaju74@gmail.com>
2023-12-13 13:47:09 +0545

    slide

--- src/components/imageSlider/Slideme.jsx
+++ src/components/imageSlider/Slideme.jsx
@@ -22,9 +22,10 @@ export default function Slideme() {
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
+    
     autoplay: true,
     speed: 5000,
-    autoplaySpeed: 500,
+    autoplaySpeed: 1,
     cssEase: "linear",
     arrows:false,
     responsive: [
@@ -32,24 +33,28 @@ export default function Slideme() {
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToScroll: 1,
             infinite: true,
-            dots: true
+            dots: false
           }
         },
         {
           breakpoint: 600,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
+            
           }
         },
         {
           breakpoint: 480,
           settings: {
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
           }
         }
       ]
@@ -58,7 +63,7 @@ export default function Slideme() {
     <Slider {...settings} className="">
       {gallaryImage.map((img) => {
         return (
-          <div key={img} className={`h-[50vh]`}>
+          <div key={img} className={`h-[40vh]`}>
             <img src={`${img}`} className="h-full object-cover       w-full" alt="" />
           </div>
         );
